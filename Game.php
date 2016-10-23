7451ec4a0ca7ae2197ec47f6ddbbb84b29d80a8a
BeastlySwagmaster <DJHam727@gmail.com>
2016-10-23 12:21:33 -0400

    Update Game.php

--- Game.php
+++ Game.php
@@ -10,7 +10,34 @@ class Game
 		$player1 = new Player("Dan");
 		$player2 = new Player("Jeremy");
 		echo "This will be a match between ". $player1->getName() . " and " . $player2->getName() . "<br>";
+		$turnCount = 0;
+		$gameOver = False;
 	}	
+	
+	public function incrementTurn()
+	{
+		do
+		{
+			$turnCount += 1;
+
+			if($turnCount % 2 == 0)
+			{
+				playTurn($player2);
+			}
+			else if ($turnCount % 2 == 1)
+			{
+				playTurn($player1);
+			}			
+			
+		} while($gameOver != True);
+	}
+	
+	public function playTurn(Player $currentPlayer)
+	{
+		$currentPlayer.deck.draw();
+		
+		
+	}
 }
 
 ?>
