b491c72a08669b2e2edb2499a6c2109e11db6415
BeastlySwagmaster <DJHam727@gmail.com>
2016-10-23 11:42:10 -0400

    Update Card.php

--- Card.php
+++ Card.php
@@ -3,34 +3,16 @@
 
 class Card 
 {
-	private $health;
-	private $image;
 	private $type;
 	private $title;
+	private $ID;
 	public function __construct($health,$image, $title, $type)
 	{
-		$this->health = $health;
-		$this->image = $image;
 		$this->title = $title;
 		$this->type = $type;
 		print "Created a new card named " . $title . " who is a " . $type . " type and has " . $health . " health <br>";
 	}
-	public function setHealth($health)
-	{
-		$this->health = $health;
-	}
-	public function getHealth()
-	{
-		return $this->health;
-	}
-	public function setImage($image)
-	{
-		$this->image = $image;
-	}
-	public function getImage()
-	{
-		return $this->image;
-	}
+
 	public function setName($title)
 	{
 		$this->name = $title;
@@ -39,8 +21,6 @@ class Card
 	{
 		return $this->name;
 	}
-	
-	
-	
 }
+
 ?>
