48575383b692626cf9386fec5dbcb48c6c89ce53
BeastlySwagmaster <DJHam727@gmail.com>
2016-10-23 12:00:44 -0400

    Update Game.php

--- Game.php
+++ Game.php
@@ -5,20 +5,13 @@ require_once 'Deck.php';
 
 class Game
 {
-	public $game;
-	public $charizard;
-	
 	public function __construct()
 	{
 		$player1 = new Player("Dan");
 		$player2 = new Player("Jeremy");
 		echo "This will be a match between ". $player1->getName() . " and " . $player2->getName() . "<br>";
-	}
-	
-	
+	}	
 }
-
-
 
 ?>
 <?php 
