e30b174b23f49faab8a324aaac6ee877db67a38f
BeastlySwagmaster <DJHam727@gmail.com>
2016-10-26 21:14:41 -0400

    Update Deck.php

--- Deck.php
+++ Deck.php
@@ -4,20 +4,49 @@ require_once 'Card.php';
 
 class Deck 
 {
-	//deck will be an array of cards
+		//deck will be an array of cards
 	private $deck = array();
-	//deck will have a size value
+		//deck will have a size value
 	private $size;
-	//temporary placeholder value for maximum size of deck
+		//temporary placeholder value for maximum size of deck
 	const MAX_SIZE = 20;
 	
 	public function __construct()
 	{
+			//If you can't connect, explode. The localhost thing is going to have to change when we get a server.
+		if ( !( $database = mysqli_connect( "localhost:8765", $_POST["Username"], $_POST["Password"], "Onslaught" ) ) )
+		{
+					die("Check your login info.");
+		}
+		
+		$count = 1;
 		$full = False;
+		
+			//This loop makes the cards.  Might have been bigger that I planned. Can make a new function.
 		do
 		{
-			$full = addCard(/*We need to retrieve info from the database*/);
+				//We update the database query each time so we can grab a different card.  
+				//This could be wrong because I didn't really follow how the database worked.
+			$query = "Select * FROM Card where Card_ID = $count";
+			
+				//If the result can't be found, explode.
+			if(!( $result = mysqli_query( $database, "$query" )))
+			{
+				die("Can't you do anything right?");
+			}
+			
+			//I believe this will give a string of the whole table row.  We need to parse it.
+			$info = mysqli_fetch_fields($result);
+		//	$health = 
+		//	$image = 
+		//	$title = 
+		//	$type = 
+				
+			$full = addCard($health, $image, $title, $type);
+		
+			$count++;
 		}while($full == False);
+		mysqli_close( $database );
 			
 		shuffle();
 	}
